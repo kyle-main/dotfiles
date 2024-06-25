9b2ccfdc4b6bc7a2e5cec06d63b07f6e078835d6
Kyle Main <kyledmain@gmail.com>
2024-06-24 21:05:38 -0400

    move plugins to seperate file.

---
+++ .config/nvim/lua/custom/globals/init.lua
@@ -0,0 +1,12 @@
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = true
+
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
