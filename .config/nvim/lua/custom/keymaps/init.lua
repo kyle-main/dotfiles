c02744e3a928bfb26b85ce01c5a366c120728938
Kyle Main <kyledmain@gmail.com>
2024-06-24 21:55:21 -0400

    more config changes.

--- .config/nvim/lua/custom/keymaps/init.lua
+++ .config/nvim/lua/custom/keymaps/init.lua
@@ -37,3 +37,7 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- Set L and H to $ and ^.
+vim.keymap.set({'n', 'x', 'o'}, 'H', '^')
+vim.keymap.set({'n', 'x', 'o'}, 'L', '$')
