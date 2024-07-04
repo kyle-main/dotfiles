9d5fe7e6582cf1d314f7505ba023a70f73e90d51
Kyle Main <kyledmain@gmail.com>
2024-07-04 16:52:04 -0400

    Add lsp restart keymap.

--- .config/nvim/lua/custom/plugins/lsp.lua
+++ .config/nvim/lua/custom/plugins/lsp.lua
@@ -97,6 +97,9 @@ return { -- LSP Configuration & Plugins
         --  For example, in C this would take you to the header.
         map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
+        -- Restart the running language server
+        map('<leader>lr', '<cmd>LspRestart<CR>', '[L]sp [R]estart')
+
         -- The following two autocommands are used to highlight references of the
         -- word under your cursor when your cursor rests there for a little while.
         --    See `:help CursorHold` for information about when this is executed
