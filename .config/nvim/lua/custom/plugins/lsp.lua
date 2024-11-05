d78d030f8580b0665faf97bf1284444d0f2cdec2
Kyle Main <kyledmain@gmail.com>
2024-11-04 21:02:54 -0500

    Add dockerls to lsp.

--- .config/nvim/lua/custom/plugins/lsp.lua
+++ .config/nvim/lua/custom/plugins/lsp.lua
@@ -173,6 +173,7 @@ return { -- LSP Configuration & Plugins
       },
       rust_analyzer = {},
       clangd = {},
+      dockerls = {},
       -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
       --
       -- Some languages (like typescript) have entire language plugins that can be useful:
