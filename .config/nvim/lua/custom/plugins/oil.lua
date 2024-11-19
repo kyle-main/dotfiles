e21d86d6ff310fece628698b5fea2703713dfae4
Kyle Main <kyledmain@gmail.com>
2024-11-19 16:17:30 -0500

    Add keymap for floating Oil window.

--- .config/nvim/lua/custom/plugins/oil.lua
+++ .config/nvim/lua/custom/plugins/oil.lua
@@ -1,7 +1,23 @@
 return {
-  'stevearc/oil.nvim',
-  opts = {},
-  -- Optional dependencies
-  dependencies = { 'echasnovski/mini.icons' },
-  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+  {
+    'stevearc/oil.nvim',
+    dependencies = { 'echasnovski/mini.icons' },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+    config = function()
+      require('oil').setup {
+        column = { 'icon' },
+        keymap = {
+          ['<C-h>'] = false,
+          ['<M-h>'] = 'actions.select_split',
+        },
+        view_options = {
+          show_hidden = true,
+        },
+      }
+      -- Open parent directory in current window
+      -- vim.keymap.set('n', '-', '<Cmd>Oil<Cr>', { desc = 'Open parent directory' })
+      -- Open parent directory in floating window
+      vim.keymap.set('n', '<leader>-', require('oil').toggle_float)
+    end,
+  },
 }
